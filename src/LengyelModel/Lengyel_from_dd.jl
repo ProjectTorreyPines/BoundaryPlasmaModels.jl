a72381d8bd8abe06aa68aae1fe5842467fa0aa25
Orso Meneghini <orso82@gmail.com>
2023-05-15 07:57:15 -0700

    fix f_omp2target_expansion

--- src/LengyelModel/Lengyel_from_dd.jl
+++ src/LengyelModel/Lengyel_from_dd.jl
@@ -2,8 +2,9 @@ function setup_model(boundary_plasma_model::LengyelModel, dd::IMAS.dd)
     eqt = dd.equilibrium.time_slice[]
     cp1d = dd.core_profiles.profiles_1d[]
 
-    hfs_sol2, lfs_sol2 = IMAS.sol(eqt, dd.wall, levels=2)
-    sol = lfs_sol2[1]
+    hfs_sol, lfs_sol = IMAS.sol(eqt, dd.wall, levels=2)
+    sol = lfs_sol[1]
+    sol2 = lfs_sol[2]
 
     boundary_plasma_model.parameters.plasma.P_SOL = IMAS.power_sol(dd.core_sources, cp1d)
     boundary_plasma_model.parameters.plasma.R_omp = sol.r[sol.midplane_index]
@@ -20,7 +21,7 @@ function setup_model(boundary_plasma_model::LengyelModel, dd::IMAS.dd)
     boundary_plasma_model.parameters.sol.f_imp = [0.02]
     boundary_plasma_model.parameters.sol.imp = [:Ne]
 
-    boundary_plasma_model.parameters.target.f_omp2target_expansion = (lfs_sol2[2].r[lfs_sol2[2].midplane_index] - lfs_sol2[1].r[lfs_sol2[1].midplane_index]) / sqrt((lfs_sol2[2].r[end] - lfs_sol2[1].r[end])^2 + (lfs_sol2[2].z[end] - lfs_sol2[1].z[end])^2)
+    boundary_plasma_model.parameters.target.f_omp2target_expansion = sqrt((sol2.r[end] - sol.r[end])^2 + (sol2.z[end] - sol.z[end])^2) / (sol2.r[sol2.midplane_index] - sol.r[sol.midplane_index])
     boundary_plasma_model.parameters.target.f_spread_pfr = 1.0
     boundary_plasma_model.parameters.target.α_sp = atan(sol.Bp[end] / sol.Bt[end])
     boundary_plasma_model.parameters.target.θ_sp = sol.strike_angles[end] # [CURRENTLY NOT USED]
