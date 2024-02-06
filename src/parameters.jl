9322099ce1208670f0524635324fc09ea7a7d4ba
TimSlendebroek <32385057+TimSlendebroek@users.noreply.github.com>
2024-02-05 16:29:04 -0800

    Enforce AbstractParameters{T}

--- src/parameters.jl
+++ src/parameters.jl
@@ -1,12 +1,12 @@
 using SimulationParameters: AbstractParameters, Entry, Switch
 
-Base.@kwdef mutable struct DivertorHeatFluxModelParameters{T} <: AbstractParameters where {T<:Real}
+Base.@kwdef mutable struct DivertorHeatFluxModelParameters{T<:Real} <: AbstractParameters{T}
     _parent::WeakRef = WeakRef(nothing)
     _name::Symbol = :not_set
     lengyel::LengyelModelParameters{T} = LengyelModelParameters{T}()
 end
 
-Base.@kwdef mutable struct DivertorHeatFluxParameters{T} <: AbstractParameters where {T<:Real}
+Base.@kwdef mutable struct DivertorHeatFluxParameters{T<:Real} <: AbstractParameters{T}
     _parent::WeakRef = WeakRef(nothing)
     _name::Symbol = :not_set
     model::Switch{Symbol} = Switch{Symbol}([:lengyel], "-", ""; default=:lengyel)
