aa48867b2722222aa404c48554c74724b7276bf9
Jerome Guterl <guterlj@fusion.gat.com>
2024-06-04 13:30:38 -0700

    newq setup

--- src/LengyelModel/Lengyel_from_dd.jl
+++ src/LengyelModel/Lengyel_from_dd.jl
@@ -34,21 +34,18 @@ end
 * anything else: strike point at the `end` location in the `sol` OpenFieldLine
 """
 function setup_model(
-    boundary_plasma_model::LengyelModel,
+    boundary_plasma_model::BoundaryPlasmaModels.LengyelModel,
+    target::IMAS.divertors__divertor___target,
     eqt::IMAS.equilibrium__time_slice,
     cp1d::IMAS.core_profiles__profiles_1d,
-    sol::Vector{IMAS.OpenFieldLine},
-    P_SOL::Real,
-    λ_omp::Real;
-    strike_index::Int,
-    imp::Vector{Symbol},
-    f_imp::Vector{<:Real},
-    f_spread_pfr::Real=1.0)
+    sol1::IMAS.OpenFieldLine;
+    impurities::Vector{Symbol},
+    impurities_fraction::Vector{<:Real},
+    heat_spread_factor::Real=1.0)
 
-    sol1 = sol[1]
-    sol2 = sol[2]
+    @assert (length(impurities) == length(impurities_fraction))
 
-    boundary_plasma_model.parameters.plasma.P_SOL = P_SOL
+    boundary_plasma_model.parameters.plasma.P_SOL = @ddtime(target.power_conducted.data) + @ddtime(target.power_convected.data)
     boundary_plasma_model.parameters.plasma.R_omp = sol1.r[sol1.midplane_index]
     boundary_plasma_model.parameters.plasma.Ip = eqt.global_quantities.ip
     boundary_plasma_model.parameters.plasma.κ = eqt.boundary.elongation
@@ -58,26 +55,17 @@ function setup_model(
 
     boundary_plasma_model.parameters.sol.n_up = cp1d.electrons.density_thermal[end]
     boundary_plasma_model.parameters.sol.T_up = cp1d.electrons.temperature[end]
-    boundary_plasma_model.parameters.sol.λ_omp = λ_omp
-    boundary_plasma_model.parameters.sol.imp = imp
-    boundary_plasma_model.parameters.sol.f_imp = f_imp
+    boundary_plasma_model.parameters.sol.λ_omp = target.two_point_model[].sol_heat_decay_length
+    boundary_plasma_model.parameters.sol.imp = impurities
+    boundary_plasma_model.parameters.sol.f_imp = impurities_fraction
 
-    # automatic strike point selection (strike_index = 0) favors the outermost strike point
-    if (strike_index == 1) || ((strike_index == 0) && (sol2.r[1] > sol2.r[end]))
-        strike_index1 = 1
-        strike_index2 = 1
-    else
-        strike_index1 = length(sol1.r)
-        strike_index2 = length(sol2.r)
-    end
-
-    boundary_plasma_model.parameters.target.f_omp2target_expansion = sqrt((sol2.r[strike_index2] - sol1.r[strike_index1])^2 + (sol2.z[strike_index2] - sol1.z[strike_index1])^2) / (sol2.r[sol2.midplane_index] - sol1.r[sol1.midplane_index])
-    boundary_plasma_model.parameters.target.R = sol1.r[strike_index1]
-    boundary_plasma_model.parameters.target.f_spread_pfr = f_spread_pfr
-    boundary_plasma_model.parameters.target.α_sp = atan(sol1.Bp[strike_index1] / sol1.Bt[strike_index1])
-    boundary_plasma_model.parameters.target.θ_sp = sol1.strike_angles[strike_index1 == 1 ? 1 : 2] # [CURRENTLY NOT USED]
+    boundary_plasma_model.parameters.target.f_omp2target_expansion = @ddtime(target.flux_expansion.data)
+    λ_target = boundary_plasma_model.parameters.sol.λ_omp * boundary_plasma_model.parameters.target.f_omp2target_expansion
+    boundary_plasma_model.parameters.target.R = @ddtime(target.wetted_area.data) / (λ_target * 2π)
+    boundary_plasma_model.parameters.target.f_spread_pfr = heat_spread_factor
+    boundary_plasma_model.parameters.target.α_sp = @ddtime(target.tilt_angle_tor.data)
+    return boundary_plasma_model.parameters.target.θ_sp = @ddtime(target.tilt_angle_pol.data)
 end
-
 # questions:
 # NO R_target ?
 # Heat flux treated not as exponential decay
