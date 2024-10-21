71abd0a8f00730582423beff923e824ea5c56bbd
Orso Meneghini <orso82@gmail.com>
2024-10-21 12:02:11 -0700

    add analytics

--- docs/make.jl
+++ docs/make.jl
@@ -18,7 +18,7 @@ end
 
 makedocs(;
     modules=[BoundaryPlasmaModels],
-    format=Documenter.HTML(),
+    format=Documenter.HTML(;analytics="G-65D8V8C8VQ"),
     sitename="BoundaryPlasmaModels",
     checkdocs=:none,
     pages=["index.md", "api.md", "License" => "license.md", "Notice" => "notice.md"]
