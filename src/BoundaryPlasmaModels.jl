1eff34607bb44812c90a184840eefb8b822bf53c
Brendan Lyons <lyonsbc@fusion.gat.com>
2025-10-16 16:44:34 -0700

    Revert "Merge pull request #40 from ProjectTorreyPines/dev"
    This reverts commit cc666471522e04e4c12169b6ce45700a98b10f69, reversing
    changes made to 230ab1d4b6c12f82f613009a9422e69c402df441.

--- src/BoundaryPlasmaModels.jl
+++ src/BoundaryPlasmaModels.jl
@@ -2,78 +2,64 @@ module BoundaryPlasmaModels
 using IMAS
 using SimulationParameters: AbstractParameters, Entry, Switch
 
-abstract type SOLBoundaryModel end
-abstract type SOLBoundaryParameters{T} <: AbstractParameters{T} end
-
-# --- backward-compat: MUST be before includes ---
-const DivertorHeatFluxModel = SOLBoundaryModel
-const DivertorHeatFluxModelParameters{T} = SOLBoundaryParameters{T} where {T}
+abstract type DivertorHeatFluxModel end
+abstract type DivertorHeatFluxModelParameters{T} <: AbstractParameters{T} end
 
 include("StangebyModel/StangebyHeatFluxModel.jl")
-include("LengyelModel/LengyelHeatFluxModel.jl")
-include("SLCoupledBoundary/SLCoupledModel.jl")   
 
+include("LengyelModel/LengyelHeatFluxModel.jl")
 
 using .StangebyHeatFluxModel
 using .LengyelHeatFluxModel
-using .SLCoupledFullModel 
 
 # --- parameters --- #
 
-Base.@kwdef mutable struct SOLBoundaryParametersShell{T<:Real} <: AbstractParameters{T}
+Base.@kwdef mutable struct DivertorHeatFluxParameters{T<:Real} <: AbstractParameters{T}
     _parent::WeakRef = WeakRef(nothing)
     _name::Symbol = :not_set
-    model::Switch{Symbol} = Switch{Symbol}([:slcoupled,:lengyel,:stangeby], "-", "SOL boundary model"; default=:slcoupled)
-    setup::SOLBoundaryParameters{T} = SLCoupledParameters{T}()
+    model::Switch{Symbol} = Switch{Symbol}([:lengyel,:stangeby], "-", ""; default=:lengyel)
+    setup::DivertorHeatFluxModelParameters{T} = LengyelModelParameters{T}()
 end
-const DivertorHeatFluxParameters = SOLBoundaryParametersShell   
 
-const _setup_dic = Dict(:slcoupled => SLCoupledParameters, :lengyel => LengyelModelParameters, :stangeby => StangebyModelParameters)
+setup_model_dic = Dict(:lengyel => LengyelModelParameters, :stangeby => StangebyModelParameters)
 
-function Base.setproperty!(par::SOLBoundaryParametersShell{T}, prop::Symbol, v) where {T}
+function Base.setproperty!(par::DivertorHeatFluxParameters{T},prop::Symbol, v) where T
     if prop == :model
-        setproperty!(getfield(par,:model), :value, v)
-        setfield!(par, :setup, _setup_dic[v]{T}())
-        println("setup model parameters for model `$v`")
-    elseoka
-        setfield!(par, prop, v)
+        setproperty!(getfield(par,:model),:value,v) 
+        setfield!(par,:setup,setup_model_dic[v]{T}())
+        println("setup model parameters for model `$v`") 
+    else
+        setfield!(par,prop,v)
     end
 end
 # --- entry point --- #
-function SOLBoundaryModel(model::Symbol)
+function DivertorHeatFluxModel(model::Symbol)
     if model == :lengyel
         return LengyelModel()
     elseif model == :stangeby
         return StangebyModel()
-    elseif model == :slcoupled
-        return SLCoupledModel()
     else
-        error("SOL boundary model `$model` is not recognized")
+        error("Divertor heat flux model `$model` is not recognized")
     end
 end
 
-SOLBoundaryModel(par::SOLBoundaryParametersShell) = SOLBoundaryModel(getfield(par, :model).value, par.setup)
+DivertorHeatFluxModel(par::DivertorHeatFluxParameters) = DivertorHeatFluxModel(par.model, par.setup)
 
-function SOLBoundaryModel(model::Symbol, setup::SOLBoundaryParameters)
-    if     model == :lengyel
-        return LengyelModel(setup::LengyelModelParameters)
+function DivertorHeatFluxModel(model::Symbol, setup::DivertorHeatFluxModelParameters)
+    if model == :lengyel
+        return LengyelModel(setup)
     elseif model == :stangeby
-        return StangebyModel(setup::StangebyModelParameters)
-    elseif model == :slcoupled
-        return SLCoupledModel(setup::SLCoupledParameters)
+        return StangebyModel(setup)
     else
-        error("SOL boundary model `$model` is not recognized")
+        error("Divertor heat flux model `$model` is not recognized")
     end
 end
 
-setup_model(model::LengyelHeatFluxModel.LengyelModel, args...; kw...) = LengyelHeatFluxModel.setup_model(model, args...; kw...)
-setup_model(model::StangebyHeatFluxModel.StangebyModel, args...; kw...) = StangebyHeatFluxModel.setup_model(model, args...; kw...)
-setup_model(model::SLCoupledFullModel.SLCoupledModel, args...; kw...) = SLCoupledFullModel.setup_model(model, args...; kw...)
+setup_model(model::LengyelModel, args...; kw...) = LengyelHeatFluxModel.setup_model(model, args...; kw...)
+setup_model(model::StangebyModel, args...; kw...) = StangebyHeatFluxModel.setup_model(model, args...; kw...)
+
 
 const document = Dict()
 document[Symbol(@__MODULE__)] = [name for name in Base.names(@__MODULE__, all=false, imported=false) if name != Symbol(@__MODULE__)]
 
 end
-
-
-
